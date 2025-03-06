abd06e3cf66dc110a03fc7d7eb18c9a93f49d719
Günji <leonneumann1997@googlemail.com>
2025-03-06 20:38:55 +0100

    Delete foodtruck directory

--- foodtruck/npc/client/client.js
+++
@@ -1,9 +0,0 @@
-import * as alt from 'alt-client';
-
-alt.log('[FOODTRUCK] Client-Skript geladen');
-
-alt.on('streamSyncedMetaChange', (entity, key, value) => {
-    if (entity instanceof alt.Ped) {
-        alt.log(`[FOODTRUCK] Ped Update: ${entity.id} - ${key}: ${value}`);
-    }
-});
